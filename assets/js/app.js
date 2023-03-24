4b526ba04bd6ee9e908825ab7ef5e611c8a985d7
claudioescobar <escobarc697@gmail.com>
2023-03-24 11:34:29 -0300

    cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,9 @@ const cantidad = document.getElementById('cantidad')
 const precioTotal = document.getElementById('precioTotal')
 const cantidadTotal = document.getElementById('cantidadTotal')
 
+
 let carrito = []
+
 
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('carrito')){
@@ -24,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
-    localStorage.setItem('carrito', JSON.stringify(carrito));
+    actualizarCarrito()
 
 })
 
@@ -56,29 +58,34 @@ botonComprar.addEventListener('click',()=>{
 } )
 
 
+
+
+
+
 stockProductos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML = `
-    <img src=${producto.img} alt= "">
-    <h3>${producto.nombre}</h3>
-    <p>Talle: ${producto.talle}</p>
-    <p class="precioProducto">Precio:$ ${producto.precio}</p>
-    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
-
-    `
-    contenedorProductos.appendChild(div)
-
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src=${producto.img} alt= "">
+        <h3>${producto.nombre}</h3>
+        <p>Talle: ${producto.talle}</p>
+        <p class="precioProducto">Precio:$ ${producto.precio}</p>
+        <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
     
-    const boton = document.getElementById(`agregar${producto.id}`)
- 
-
-    boton.addEventListener('click', () => {
+        `
+        contenedorProductos.appendChild(div)
+    
         
-        agregarAlCarrito(producto.id)
-        
+        const boton = document.getElementById(`agregar${producto.id}`)
+     
+    
+        boton.addEventListener('click', () => {
+            
+            agregarAlCarrito(producto.id)
+            
+        })
     })
-})
+
 
 
 const agregarAlCarrito = (prodId) => {
